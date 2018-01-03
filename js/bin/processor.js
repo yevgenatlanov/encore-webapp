37d8658ada43e47ddc10315ce8a1583bfbf29ac5
Yevgen Atlanov <yevgen@Yevgens-MacBook-Pro.local>
2018-01-03 09:43:02 +0200

    project rework

--- js/bin/processor.js
+++ js/bin/processor.js
@@ -2,7 +2,10 @@
 $(document).ready(function () {
 
     //ok here we go :)
-    $(".dropdown-button").dropdown();
+    // $(".dropdown-button").dropdown();
+    //$(".dropdown-button").dropdown();
+
+
 
     $.getJSON("../../app.json", function (json) {
         //console.log(json);
@@ -11,16 +14,16 @@ $(document).ready(function () {
         // some magic here 
         // first off all we need to render header and home page
 
-        $("#logo-container img").attr("src", json.app_logo);
+
         // now we move to nav
         var pages = json.pages;
-        for (var i = 0; i < pages.length; i++) {
-            if (pages[i].name === "Products") {
-                $("#navigation").append("<li data-id=" + i + "><a class='dropdown-button' data-activates='dropdown1' href='#!'>" + pages[i].name + "</a></li>");
-            } else {
-                $("#navigation").append("<li data-id=" + i + "><a>" + pages[i].name + "</a></li>");
-            }
-        }
+        // for (var i = 0; i < pages.length; i++) {
+        //     if (pages[i].name === "Products") {
+        //         $("#navigation").append("<li data-id=" + i + "><a class='dropdown-button' data-activates='dropdown1' href='#!'>" + pages[i].name + "</a></li>");
+        //     } else {
+        //         $("#navigation").append("<li data-id=" + i + "><a>" + pages[i].name + "</a></li>");
+        //     }
+        // }
 
         // and now we can render home page :)
         pageRender(0);
@@ -42,10 +45,15 @@ $(document).ready(function () {
             $("#preloader")
                 .delay(1000)
                 .fadeOut();
+
+
         });
 
         // functions 
         function pageRender(id) {
+            $("#magic-nav").load("nav.html");
+            $("#magic-footer").load("footer.html");
+
             $("#navigation li").removeClass("active");
             $("#navigation li[data-id=" + id + "]").addClass("active");
 
@@ -56,6 +64,7 @@ $(document).ready(function () {
 
             var sections = page.sections;
             var collections = json.collections;
+
             for (var j = 0; j < sections.length; j++) {
 
                 var section = page.sections[j];
@@ -68,16 +77,64 @@ $(document).ready(function () {
                 }
 
                 // section collections update
-                   
+
                 if (section.type === "collections") {
                     for (var c = 0; c < collections.length; c++) {
                         var collection = collections[c];
-                        $("#" + collection.slug +" .header").html(collection.title);
-                        $("#" + collection.slug +" p").html(collection.description);
-                        $("#" + collection.slug +" img").attr("src", "assets/dev/collection-headers/" + collection.mainImage);                        
+                        $("#" + collection.slug + " .header").html(collection.title);
+                        $("#" + collection.slug + " p").html(collection.description);
+                        $("#" + collection.slug + " img").attr("src", "assets/dev/collection-headers/" + collection.mainImage);
                     }
                 }
             }
         }
+
+
+        // lets try to echo collection
+
+        var coll = json.collections;
+
+        var title = $(".title-area b").html();
+
+        for (var c = 0; c < coll.length; c++) {
+            var items = coll[c];
+            if (title === items.title) {
+                var products = items.categories[0].items;
+              
+                for (var it = 0; it < products.length; it++) {
+
+                    var item = products[it];
+                  
+                  
+                    if (products.lengt <= 4 ) {
+                        $("#VintageCuriosity .items").append(`<div data-id="${it}" class="item"><div class="col s3"><div class="card"><div class="card-image parallax"><img  class="materialboxed parallax" data-caption="${item.name}" src="assets/${item.image}"></div><div class="card-content"><p>${item.name}<br>${item.size}</p></div></div></div></div>`);
+                    
+                    } else {
+                        $("#VintageCuriosity .items").append(`<div data-id="${it}" class="item"><div class="col s2"><div class="card"><div class="card-image parallax"><img class="materialboxed parallax" data-caption="${item.name}" src="assets/${item.image}"></div><div class="card-content"><p>${item.name}<br>${item.size}</p></div></div></div></div>`);
+                    }
+
+
+
+                }
+
+                var Lithia = items.categories[1].items;
+                
+                for (var lc = 0; lc < Lithia.length; lc++) {
+
+                    var litem = Lithia[lc];
+                  
+                  
+                 
+                    $("#LithiaPark .items").append(`<div data-id="${lc}" class="item"><div class="col s2"><div class="card"><div class="card-image"><img  class="materialboxed " data-caption="${litem.name}" src="assets/${litem.image}"></div><div class="card-content"><p>${litem.name}<br>${litem.size}</p></div></div></div></div>`);
+                    
+                   
+
+
+
+                }
+            }
+
+        }
+
     });
 });
